1779e408e92a48c4004c07b2d694da52a9b839e5
Jesse Moody <david.jesse@moodyiii.com>
2016-07-17 23:29:25 -0400

    logo

--- js/freelancer.js
+++ js/freelancer.js
@@ -2,7 +2,7 @@
 
 (function($) {
     "use strict"; // Start of use strict
-
+    
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
@@ -11,26 +11,28 @@
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-
+    
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
     });
-
+    
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
         $('.navbar-toggle:visible').click();
     });
-
+    
+    
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
             top: 100
         }
-    })
-
-    // Floating label headings for the contact form
+        
+    });
+    
+    //Floating label headings for the contact form
     $(function() {
         $("body").on("input propertychange", ".floating-label-form-group", function(e) {
             $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
